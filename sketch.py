685dc6657f6d3f64e5cdd8084b17a79ad3e61d07
JAZIEL LOPEZ <jaziel.lopez@thermofisher.com>
2021-07-18 23:22:09 -0700

    smoothness sketch resulting image

--- sketch.py
+++ sketch.py
@@ -20,12 +20,15 @@ sketch_path = os.path.join(directory, sketch_filename)
 # print(directory)
 # print(sketch_filename)
 
-sk_gray, sk_color=cv2.pencilSketch(cv2.imread(source), sigma_s=60, sigma_r=0.07, shade_factor=0.05)
+sk_gray, sk_color=cv2.pencilSketch(cv2.imread(source), 
+        sigma_s=10, 
+        sigma_r=0.15,
+        shade_factor=0.099)
 cv2.imwrite(sketch_path, sk_gray)
 
 print("sketch version of your image written to: {}".format(sketch_path))
-print("DO NOT CLOSE THE WINDOW....using your keyboard press any key to exit gracefully")
-cv2.imshow(sketch_filename, cv2.imread(sketch_path))
-cv2.waitKey(0)
+# print("DO NOT CLOSE THE WINDOW....using your keyboard press any key to exit gracefully")
+# cv2.imshow(sketch_filename, cv2.imread(sketch_path))
+# cv2.waitKey(0)
 
 print("good bye...")
